20a308c6b51f8d155fd063d85adfe7a3c808ffa8
nlantau <niklaslantau@gmail.com>
2021-02-19 00:17:34 +0100

    minor changes

--- Rust/codewars_2021/src/main.rs
+++ Rust/codewars_2021/src/main.rs
@@ -1,8 +1,16 @@
 fn main() {
     let t: Vec<usize> = generate_range(2, 10, 2);
     println!("Vec t: {:?}", t);
+
+    println!("Is divide by: {}", is_divide_by(8,3,4));
 }
 
+// Completed
+fn is_divide_by(n: i32, a: i32, b: i32) -> bool {
+    (n % a == 0) & (n % b == 0)
+}
+
+// Completed
 fn generate_range(min: usize, max: usize, step: usize) -> Vec<usize> {
     let mut vec: Vec<usize> = Vec::new();
     let mut i: usize = min;
