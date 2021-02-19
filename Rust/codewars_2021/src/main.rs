7b586138f395ac174dce1da6019b400641620201
nlantau <niklaslantau@gmail.com>
2021-02-19 01:10:38 +0100

    minor changes

--- Rust/codewars_2021/src/main.rs
+++ Rust/codewars_2021/src/main.rs
@@ -3,7 +3,14 @@ fn main() {
     println!("Vec t: {:?}", t);
 
     println!("Is divide by: {}", is_divide_by(8,3,4));
+
+    println!("bin to dec: {}", bin_to_decimal("1001001"));
 }
+
+fn bin_to_decimal(inp: &str) -> i32 {
+    i32::from_str_radix(inp, 2).unwrap()
+}
+
 
 // Completed
 fn is_divide_by(n: i32, a: i32, b: i32) -> bool {
