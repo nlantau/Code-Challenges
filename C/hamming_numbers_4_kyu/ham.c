b0db46099fe9a42666fc40e3762bafb4994c599c
Niklas Lantau <niklaslantau@gmail.com>
2021-09-04 15:43:56 +0200

    update

---
+++ C/hamming_numbers_4_kyu/ham.c
@@ -0,0 +1,19 @@
+/* Hamming Numbers - 4 kyu
+ * nlantau, 2021-09-04
+ */
+#include <stdio.h>
+//#include <stdlib.h>
+#include <stdint.h>
+
+uint64_t hamber(int n)
+{
+
+	// Continue some other time..
+	return 0;
+}
+
+int main(void)
+{
+	printf("hamber() return: %ld\n", hamber(1));
+	return 0;
+}
