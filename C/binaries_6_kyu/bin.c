bb0fd8e19a8ebb61a6721225a96e8fe8dd35eb6b
Niklas Lantau <niklaslantau@gmail.com>
2021-09-05 22:19:47 +0200

    Binaries: decode() real code begun

--- C/binaries_6_kyu/bin.c
+++ C/binaries_6_kyu/bin.c
@@ -34,46 +34,6 @@ char *bin(char c)
 	return x;
 }
 
-char *decode(const char *s)
-{
-	/*
-	 * Given a binary string (encoded by code()),
-	 * decode it to base 10.
-	 *
-	 * # Stage 1
-	 * 1. Start from MSB.
-	 * 2. Look at first digit
-	 * 	a. if '1' -> Could be digit 0 or 1
-	 * 	b. else 
-	 * 		look at pos 1 and pos 2:
-	 * 		if pos1 == 0 and pos2 == 1 -> Could be digit 2 or 3
-	 * 		else if pos1 == 0 and pos2 == 0 -> Could be digit 4,5,6 or 7
-	 * 			if not 4,5,6 ot 7
-	 * 				look at pos3
-	 * 				if pos3 == 0 -> either digit 8 or 9
-	 * 					LSB == 0 -> digit 8
-	 * 					LSB == 1 -> digit 9
-	 *
-	 * # Stage 2
-	 * 1. Save the found/matching digit.
-	 * 2. Trim down given string (remove binary part from stage 1)
-	 * 3. Repeat stage 1 for next digit
-	 *
-	 * code(0) --> 10
-	 * code(1) --> 11
-	 * code(2) --> 0110
-	 * code(3) --> 0111
-	 * code(4) --> 001100
-	 * code(5) --> 001101
-	 * code(6) --> 001110
-	 * code(7) --> 001111
-	 * code(8) --> 00011000
-	 * code(9) --> 00011001
-	 *
-	 */
-
-	return "decode";
-}
 
 char *code(const char *org)
 {
@@ -151,25 +111,101 @@ char *code(const char *org)
 	return s2;
 }
 
+char *decode(const char *org)
+{
+	/*
+	 * Given a binary string (encoded by code()),
+	 * decode it to base 10.
+	 *
+	 * # Stage 1
+	 * 1. Start from MSB.
+	 * 2. Look at first digit
+	 * 	a. if '1' -> Could be digit 0 or 1
+	 * 	b. else 
+	 * 		look at pos 1 and pos 2:
+	 * 		if pos1 == 0 and pos2 == 1 -> Could be digit 2 or 3
+	 * 		else if pos1 == 0 and pos2 == 0 -> Could be digit 4,5,6 or 7
+	 * 			if not 4,5,6 ot 7
+	 * 				look at pos3
+	 * 				if pos3 == 0 -> either digit 8 or 9
+	 * 					LSB == 0 -> digit 8
+	 * 					LSB == 1 -> digit 9
+	 *
+	 * # Stage 2
+	 * 1. Save the found/matching digit.
+	 * 2. Trim down given string (remove binary part from stage 1)
+	 * 3. Repeat stage 1 for next digit
+	 *
+	 * code(0) --> 10
+	 * code(1) --> 11
+	 * code(2) --> 0110
+	 * code(3) --> 0111
+	 * code(4) --> 001100
+	 * code(5) --> 001101
+	 * code(6) --> 001110
+	 * code(7) --> 001111
+	 * code(8) --> 00011000
+	 * code(9) --> 00011001
+	 *
+	 */
+	char *s = calloc(250, sizeof(char)), *sp;
+	char *t = calloc(250, sizeof(char)), *tp, *orgp;
+
+	sp = s;
+	tp = s;
+	orgp = org;
+	
+	int i = 0;
+	int len = strlen(org);
+
+	if (len < 1) return s;
+
+	for (;*org != '\0';) {
+		if (*org == 49) {
+			*(org + 1) == 48 ? sprintf(s, "%c", 48) : sprintf(s, "%c", 49);
+			*org++;
+			*org++;
+		} else {
+			*org++;
+		}
+	}
+
+
+
+	org = orgp;
+	s = tp;
+	free(t); t = NULL;
+	return s;
+}
+
 int main(void)
 {
+	/*
 	int i = 0;
 	char s[4];
 	for (; i < 10; i++) {
 		sprintf(s, "%d", i);
 		printf("code(%d) --> %s\n", i, code(s));
 	}
+	*/
 
+	char *a = decode("10");
+	printf("0:%s\n", a);
+	free(a); a = NULL;
+
+	char *b = decode("11");
+	printf("1:%s\n", b);
+	free(b); b = NULL;
+
+	char *c = decode("0110");
+	printf("2:%s\n", c);
+	free(c); c = NULL;
+
+
+
+	
 	//printf("%s\n", code("77338855"));
 	//printf("%s\n", code("77338"));
 	//printf("%s\n", code("0011121314"));
-	//printf("%s\n", decode("0"));
-	//printf("%s\n", decode("2"));
-	//printf("%s\n", decode("3"));
-	//printf("%s\n", decode("4"));
-	//printf("%s\n", decode("5"));
-	//printf("%s\n", decode("123"));
-	//printf("%s\n", decode("77338"));
-	//printf("%s\n", decode("2"));
 	return 0;
 }
