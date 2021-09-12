88d7337c2b7c7cc6edf16ab0278510f22b21f41a
Niklas Lantau <niklaslantau@gmail.com>
2021-09-12 17:28:40 +0200

    Consecutive strings: It works on my end, but crashes (free error) on cw tests

--- C/consecutive_strings/con.c
+++ C/consecutive_strings/con.c
@@ -18,6 +18,9 @@ int main(void)
 	
 	char* arr[] = {"itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"};
 	// arr, 3, 2 --> "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck"
+	
+	char* brr[] = {};
+	// brr, 0, 3 --> ""
 
 	char *s = longestConsec(a1, 8, 2);
 	printf("[%s]\n", s);
@@ -30,30 +33,40 @@ int main(void)
 	s = longestConsec(arr, 3, 2);
 	printf("[%s]\n", s);
 	free(s); s = NULL;
+
+	s = longestConsec(brr, 0, 3);
+	printf("[%s]\n", s);
+	free(s); s = NULL;
+
+	s = longestConsec(arr, 3, 2);
+	printf("[%s]\n", s);
+	free(s); s = NULL;
 	return 0;
 }
 
 
 char *longestConsec(char *sarr[], int n, int k)
-{
-	int i = 0, j = 0;
-	char *t = calloc(100, sizeof(char));
-	char *s = calloc(100, sizeof(char));
+{  
 
+	int i = 0, j = 0, c = 1, csize = 0;
+	char **t = NULL;
+	char *s = calloc(1, sizeof(char));
+	if ((n == 0) || (k > n) || (k <= 0)) {
+		strcpy(s, "");
+		return s;
+	}
 	for (; i < n; i++) {
-		memset(t, '\0', strlen(t));
-		for (j = i; j != k+i; j++) {
-			t = realloc(t, strlen(sarr[j])* 100 * sizeof(char));
-			strcat(t, sarr[j]);
-		}
-		printf("t = [%s]\n", t);
-		if (strlen(t) > strlen(s)) {
-			s = realloc(s, strlen(t) * sizeof(char) );
-			memset(s, '\0', strlen(s));
-			strcpy(s, t);
+		csize = 0;
+		for (j = i; j != k+1 && j != n; j++) csize += strlen(sarr[j]);
+		t = realloc(t, sizeof(char*) * ++c);
+		t[i] = calloc(1, csize + 1);
+		for (j = i; j != k+i && j != n; j++) strcat(t[i], sarr[j]);
+		if (strlen(t[i]) > strlen(s)) {
+			s = realloc(s, strlen(t[i]) * sizeof(char) );
+			strcpy(s, t[i]);
 		}
 	}
-
+	for (i = 0; i < n; i++) free(t[i]);
 	free(t); t = NULL;
 	return s;
 }
