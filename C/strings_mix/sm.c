0265a331725bcfd2ebb20ddff3c045adb19fa159
Niklas Lantau <niklaslantau@gmail.com>
2021-09-13 17:36:21 +0200

    Strings Mix: 4 kyu: No progress. TODO: Last part

--- C/strings_mix/sm.c
+++ C/strings_mix/sm.c
@@ -10,8 +10,15 @@ typedef struct {
 	int dig;
 }cfreq;
 
+typedef struct {
+	char num_or_sign;
+	char chr;
+	int len;
+} c_table;
+
 char* mix(char* s1, char* s2);
 void print_struct(cfreq *s, cfreq *t);
+void print_ctable(c_table *s);
 
 int main(void)
 {
@@ -32,12 +39,7 @@ int main(void)
 char* mix(char* s1, char* s2)
 {
 
-	typedef struct {
-		char chr;
-		int length;
-	} c_table;
 
-	c_table c_t[2];
 	char *ps1 = s1, *ps2 = s2;
 	cfreq ss1[26];
 	cfreq ss2[26];
@@ -74,22 +76,11 @@ char* mix(char* s1, char* s2)
 
 		// Only 'strictly greater than 1 is interesting
 		if (ss1[i].dig > 1 || ss2[i].dig > 1) {
-			printf("ss1[%d].dig = [%d]\n", i, ss1[i].dig);
-			printf("ss2[%d].dig = [%d]\n", i, ss2[i].dig);
 
-
-			for (int j = 0; j < ss1[i].dig; j++) {
+			for (int j = 0; j < ss1[i].dig; j++)
 				*s1ct++ = ss1[i].chr;
-				printf("*s1ct = [%s]\n", s1ctp);
-			}
 			for (int j = 0; j < ss2[i].dig; j++)
 				*s2ct++ = ss2[i].chr;
-				printf("*s2ct = [%s]\n", s1ctp);
-			//printf("s1ct = [%s]\n", s1ct);
-			//printf("s2ct = [%s]\n", s2ct);
-
-			//printf("len: s1ct=[%d], s2ct[%d]\n",\
-					strlen(s1ct), strlen(s2ct));
 
 			s1ct = s1ctp;
 			s2ct = s2ctp;
@@ -118,14 +109,55 @@ char* mix(char* s1, char* s2)
 
 	s1ct = s1ctp;
 	s2ct = s2ctp;
+
+	// String count thing is complete
+	// Now - need to put in correct order
+	
+	// s1c is our string.
+	//s1cp = s1c;
+	
+	printf("s1c  = [%s]\n", s1cp);
+	printf("s1c  = [%s]\n", s1c);
+	
+	c_table c_o[26];
+
+	/* c_o[0].num_or_sign = '1', '2' or '='
+	 * c_o[0].chr         = 'a' -> 'z'
+	 * c_o[0].len         = number of letters until '/'
+	 */
+
+	for (int i = 0; i < 26, *s1c != '\0'; i++) {
+		//printf("*s1c = [%c]\n", *s1c);
+		c_o[i].num_or_sign = *s1c;
+		c_o[i].chr = *(s1c +2);
+		c_o[i].len = 1;
+		while(*s1c == *(s1c+1)) {
+			c_o[i].len++;
+			*s1c++;
+		}
+		c_o[i].len++;
+	}
+	print_ctable(c_o);
+
+	s1c = s1cp;
 	printf("s1c  = [%s]\n", s1cp);
 	printf("s1c  = [%s]\n", s1c);
 	printf("s1ct = [%s]\n", s1ctp);
 	printf("s2ct = [%s]\n", s2ctp);
-	free(s1ctp); s1ctp = NULL;
-	free(s2ctp); s2ctp = NULL;
+	free(s1ct); s1ct = NULL;
+	free(s2ct); s2ct = NULL;
 	
 	return s1cp;
+}
+
+void print_ctable(c_table *s)
+{
+	for (int i = 0; i < 26; i++) {
+		printf("c_o[%2d].nos = [%2c] | c_o[%2d]."
+		       "chr = [%2c] | c_o[%2d].len = [%2d]\n", 
+		       i, s[i].num_or_sign,
+		       i, s[i].chr, i, s[i].len);
+	}
 }
 
 void print_struct(cfreq *s, cfreq *t)
