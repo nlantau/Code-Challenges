9bca74c4f5a057b5f622b735f64ee3547cc2c553
Niklas Lantau <niklaslantau@gmail.com>
2021-11-26 03:27:03 +0100

    update

--- Rust/rust_2021/src/d1_2015.rs
+++ Rust/rust_2021/src/d1_2015.rs
@@ -1,6 +1,24 @@
 use std::fs;
 
-pub fn day1_2015() -> i32 {
+pub fn day1_2015b() -> (i32, usize) {
+    let line: String = read_data();
+    let mut score: i32 = 0;
+    let mut res: usize = 0;
+    let mut base: bool = false;
+
+    for (i, ch) in line.chars().enumerate() {
+        score = if ch == '(' { score + 1 } else { score - 1 };
+        if score == -1 && !base {
+            res = i + 1;
+            base = true;
+        }
+    }
+    (score, res)
+}
+
+// Don't really need this function.
+// Keeping it for learning purposes
+pub fn day1_2015a() -> i32 {
     let line = read_data();
     line
         .chars()
--- Rust/rust_2021/src/main.rs
+++ Rust/rust_2021/src/main.rs
@@ -1,13 +1,16 @@
-use rust_2021::d1_2015::day1_2015;
+use rust_2021::d1_2015::{day1_2015a, day1_2015b};
 use rust_2021::day1::day1a;
 
 fn main() {
-    let args = std::env::args().collect::<Vec<String>>();
-    let problem = args.get(1).map(|s| s.as_str()).unwrap_or("None");
+    let args: Vec<String> = std::env::args().collect::<Vec<String>>();
+    let problem: &str = args.get(1).map(|s| s.as_str()).unwrap_or("None");
 
-    let result = match problem {
+    let result: String = match problem {
         "day1a" => day1a(),
-        "d1_2015" => day1_2015().to_string(),
+        "d1_2015a" => day1_2015a().to_string(),
+        "d1_2015b" => day1_2015b().0.to_string()
+            + ", "
+            + &*day1_2015b().1.to_string(),
         _ => "What?".to_string(),
     };
     println!("{}", result);
