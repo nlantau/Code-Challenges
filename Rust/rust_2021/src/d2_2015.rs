86a5c1a2041aec481859e23ae6fcdfb68504c7bd
Niklas Lantau <niklaslantau@gmail.com>
2021-11-27 00:16:03 +0100

    update

--- Rust/rust_2021/src/d2_2015.rs
+++ Rust/rust_2021/src/d2_2015.rs
@@ -7,17 +7,15 @@ use std::path::Path;
 pub fn day2() -> String {
     let lines: Vec<String> = read_data_d2();
     lines.get(0).unwrap().to_string()
-
 }
-
 
 
 fn read_data_d2() -> Vec<String> {
     let inp = File::open(Path::new("assets/d2_2015.txt")).unwrap();
     let reader = BufReader::new(&inp);
-    let lines : Vec<String>= reader
+    let lines: Vec<String> = reader
         .lines()
-        .collect::<Result<_,_>>()
+        .collect::<Result<_, _>>()
         .unwrap();
     lines
 }
