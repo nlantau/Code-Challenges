2a5b4d5b32ed46a48705e18300e60ae6d4d5ff98
Niklas Lantau <niklaslantau@gmail.com>
2021-11-27 02:17:23 +0100

    update

--- Rust/rust_2021/src/d2_2015.rs
+++ Rust/rust_2021/src/d2_2015.rs
@@ -1,14 +1,33 @@
+use std::cmp::min;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::path::Path;
 
-
 pub fn day2() -> String {
     let lines: Vec<String> = read_data_d2();
-    lines.get(0).unwrap().to_string()
+    let res: i32 = lines
+        .iter()
+        .map(|s| str_vec_to_int(s))
+        .map(wrapping_paper)
+        .sum();
+
+    res.to_string()
 }
 
+fn wrapping_paper(numbs: Vec<i32>) -> i32 {
+    let lw = numbs[0] * numbs[1];
+    let lh = numbs[0] * numbs[2];
+    let wh = numbs[1] * numbs[2];
+    2 * lw + 2 * wh + 2 * lh + min(lw, min(lh, wh))
+}
+
+fn str_vec_to_int(line: &str) -> Vec<i32> {
+    line.split('x')
+        .map(|d| d.parse())
+        .map(|d| d.unwrap())
+        .collect()
+}
 
 fn read_data_d2() -> Vec<String> {
     let inp = File::open(Path::new("assets/d2_2015.txt")).unwrap();
