dbbd00f1f2a205d4b4b0c83701d69045bd4b6cd8
Niklas Lantau <niklaslantau@gmail.com>
2021-12-10 17:52:26 +0100

    update

--- Rust/rust_2021/src/d10_2021.rs
+++ Rust/rust_2021/src/d10_2021.rs
@@ -3,7 +3,8 @@ use std::collections::HashMap;
 use std::time;
 
 pub fn d10_2021_solution() {
-    bench(part1);
+    //bench(part1);
+    bench(part2);
 }
 
 fn bench(f: fn()) {
@@ -13,12 +14,15 @@ fn bench(f: fn()) {
     ret
 }
 
-fn part1() {
-    let inp: Vec<Vec<char>> = include_str!("../assets/d10_2021.txt").lines().collect::<Vec<&str>>()
+fn get_data() -> Vec<Vec<char>> {
+    include_str!("../assets/d10_2021_test.txt").lines().collect::<Vec<&str>>()
         .iter()
         .map(|&v| v.chars().collect::<Vec<char>>())
-        .collect();
+        .collect()
+}
 
+fn part1() {
+    let inp = get_data();
     let error_pairs: Vec<(char, char)> = syntax_checker(&inp);
     let err_sum: HashMap<char, Vec<u32>> = calculate_syntax_score(&error_pairs);
     let mut res = 0;
@@ -26,7 +30,90 @@ fn part1() {
         res += v[0] * v.len() as u32;
     }
     println!("Part 1: {}", res);
-    assert_eq!(res, 243939);
+    assert_eq!(res, 26397);
+    //assert_eq!(res, 243939);
+}
+
+fn part2() {
+    let inp = get_data();
+    let errs = syntax_repair2(&inp);
+}
+
+#[derive(Debug)]
+struct Token {
+    token: char,
+    expected_closer: char,
+    found_closer: char,
+    complete: bool,
+    position: i32,
+}
+
+impl Token {
+    fn new() -> Token {
+        Token {
+            token: '=',
+            expected_closer: '?',
+            found_closer: '!',
+            complete: false,
+            position: -1,
+        }
+    }
+
+    fn set_complete(&mut self) {
+        let b = self.expected_closer == self.found_closer;
+        self.complete = b;
+    }
+}
+
+fn syntax_repair2(inp: &[Vec<char>]) -> Vec<(char, char)> {
+    let mut error_pairs: Vec<(char, char)> = Vec::new();
+    let mut stack: Vec<char> = Vec::new();
+    let mut missing: Vec<char> = Vec::new();
+
+    for v in inp.iter() {
+        for c in v.iter() {
+            if is_opener(c) {
+                stack.push(*c);
+            } else if is_closer(c) {
+                let open_token = stack.pop().unwrap();
+
+                if open_token == '(' && *c != ')' {
+                    //error_pairs.push((')', *c));
+                    missing.push(')');
+                } else if open_token == '[' && *c != ']' {
+                    //error_pairs.push((']', *c));
+                    missing.push(']');
+                } else if open_token == '{' && *c != '}' {
+                    //error_pairs.push(('}', *c));
+                    missing.push('}');
+                } else if open_token == '<' && *c != '>' {
+                    //error_pairs.push(('>', *c));
+                    missing.push('>');
+                }
+            }
+        }
+    }
+    missing.push('-');
+    missing.iter().for_each(|c| println!("{}", c));
+    error_pairs
+}
+
+#[allow(unused_mut)]
+fn syntax_repair(inp: &[Vec<char>]) -> Vec<Token> {
+    let mut stack: Vec<char> = Vec::new();
+    let mut tokens: Vec<Token> = Vec::new();
+
+    for v in inp.iter() {
+        for (idx, opener) in v.iter().enumerate() {
+            if is_opener(opener) {
+                let mut boken: Token = Token::new();
+                boken.token = *opener;
+                tokens.push(boken);
+            }
+        }
+    }
+    dbg!(&tokens);
+    tokens
 }
 
 fn syntax_checker(inp: &[Vec<char>]) -> Vec<(char, char)> {
