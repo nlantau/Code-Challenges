507ea81500a92c63fb09c2ea173f2c38f78b3fec
Niklas Lantau <niklaslantau@gmail.com>
2021-12-11 02:41:59 +0100

    update

--- Rust/rust_2021/src/d10_2021.rs
+++ Rust/rust_2021/src/d10_2021.rs
@@ -1,10 +1,10 @@
-use std::collections::hash_map::Entry;
+use std::collections::hash_map::{Entry, VacantEntry};
 use std::collections::HashMap;
 use std::time;
 
 pub fn d10_2021_solution() {
-    //bench(part1);
-    bench(part2);
+    bench(part1);
+    //bench(part2);
 }
 
 fn bench(f: fn()) {
@@ -21,6 +21,12 @@ fn get_data() -> Vec<Vec<char>> {
         .collect()
 }
 
+fn part2() {
+    let inp = get_data();
+    inp.iter().for_each(|l| println!("{:?}", l));
+    let errs = syntax_repair(&inp);
+}
+
 fn part1() {
     let inp = get_data();
     let error_pairs: Vec<(char, char)> = syntax_checker(&inp);
@@ -34,61 +40,42 @@ fn part1() {
     //assert_eq!(res, 243939);
 }
 
-fn part2() {
-    let inp = get_data();
-    let errs = syntax_repair(&inp);
-}
-
-
-#[allow(unused_mut)]
-fn syntax_repair(inp: &[Vec<char>]) -> Vec<(char, char)> {
-    let mut error_pairs: Vec<(char, char)> = Vec::new();
-    let mut stack: Vec<char> = Vec::new();
-    let mut missing: Vec<char> = Vec::new();
-
-    for v in inp.iter() {
-        for c in v.iter() {
-            match c {
-                '(' => stack.push(*c),
-                '[' => stack.push(*c),
-                '{' => stack.push(*c),
-                '<' => stack.push(*c),
-                _ => panic!(),
-            }
-        }
-    }
-
-    missing.push('-');
-    missing.iter().for_each(|c| println!("{}", c));
-    error_pairs
-}
-
-
 fn syntax_checker(inp: &[Vec<char>]) -> Vec<(char, char)> {
     let mut error_pairs: Vec<(char, char)> = Vec::new();
     let mut stack: Vec<char> = Vec::new();
-    for v in inp.iter() {
+    let mut corrupted_lines: Vec<usize> = Vec::new();
+
+    for (i, v) in inp.iter().enumerate() {
         for c in v.iter() {
             if is_opener(c) {
                 stack.push(*c);
             } else if is_closer(c) {
                 let open_token = stack.pop().unwrap();
+
                 if open_token == '(' && *c != ')' {
                     error_pairs.push((')', *c));
-                    break;
+                    corrupted_lines.push(i);
                 } else if open_token == '[' && *c != ']' {
                     error_pairs.push((']', *c));
-                    break;
+                    corrupted_lines.push(i);
                 } else if open_token == '{' && *c != '}' {
                     error_pairs.push(('}', *c));
-                    break;
+                    corrupted_lines.push(i);
                 } else if open_token == '<' && *c != '>' {
                     error_pairs.push(('>', *c));
-                    break;
+                    corrupted_lines.push(i);
                 }
             }
         }
     }
+    /*
+    TODO: remove corrupted_lines from input => incomplete lines
+    TODO: new loop -> push if opener, pop if closer
+    TODO: reverse list or deque (?) while awarding points
+    TODO: Once solved, Rustify the solution (if let, while let, match)
+     */
+    corrupted_lines.iter().for_each(|c| print!("{},", c));
+    println!();
     error_pairs
 }
 
@@ -117,6 +104,29 @@ fn calculate_syntax_score(ep: &[(char, char)]) -> HashMap<char, Vec<u32>> {
         }
     }
     err_sum
+}
+
+#[allow(unused_mut)]
+fn syntax_repair(inp: &[Vec<char>]) -> Vec<(char, char)> {
+    let mut error_pairs: Vec<(char, char)> = Vec::new();
+    let mut stack: Vec<char> = Vec::new();
+    let mut missing: Vec<char> = Vec::new();
+
+    for v in inp.iter() {
+        for c in v.iter() {
+            match c {
+                '(' => stack.push(*c),
+                '[' => stack.push(*c),
+                '{' => stack.push(*c),
+                '<' => stack.push(*c),
+                _ => panic!(),
+            }
+        }
+    }
+
+    missing.push('-');
+    missing.iter().for_each(|c| println!("{}", c));
+    error_pairs
 }
 
 fn is_opener(c: &char) -> bool {
