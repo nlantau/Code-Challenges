3ff6fe65eaea8ceb54d9edc9c545eb5d4254dece
Niklas Lantau <niklaslantau@gmail.com>
2021-12-16 14:03:19 +0100

    update

--- Rust/rust_2021/python/d15_2021.py
+++ Rust/rust_2021/python/d15_2021.py
@@ -1,14 +1,23 @@
 # nlantau, 2021-12-15
 
+from queue import PriorityQueue
+
+
 sample = r'../assets/d15_2021_test.txt'
 data   = r'../assets/d15_2021.txt'
 
 
 with open(sample, 'r') as r:
-    data = r.read().split()
+    data = [[int(i) for i in row] for row in r.read().split()]
+    #data = r.read().split()
 
-data = [[int(i) for i in row] for row in data]
 
 print(*data, sep='\n')
+
+pq = PriorityQueue()
+
+
+
+
 
 # Implement Djikstra's or A* 
