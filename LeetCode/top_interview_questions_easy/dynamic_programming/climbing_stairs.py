95cdfd92e3c9108989908bb1b08078e301a2bf91
nlantau <niklaslantau@gmail.com>
2022-01-03 18:22:56 +0100

    minor changes

---
+++ LeetCode/top_interview_questions_easy/dynamic_programming/climbing_stairs.py
@@ -0,0 +1,39 @@
+# nlantau, 2022-01-03
+
+"""
+You are climbing a staircase. It take n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways
+can you climb to the top?
+
+Solution:
+input: n = 2
+output: 2
+1. 1 step + 1 step
+2. 2 step
+Total: 2 distinct ways
+
+input: n = 3
+output: 3
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 step + 1 step
+Total: 3 distinct ways
+
+"""
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        first, sec = 1, 1
+        for i in range(n-1):
+            first, sec = first + sec, first
+        return first
+
+"""
+Notes:
+67 ms, super-slow, 14.2 MB, 72.59% better
+"""
+
+if __name__ == "__main__":
+    print(Solution().climbStairs(3))
+    print(Solution().climbStairs(45))
+        
