46e374bf29011c76a0b7cdad75358c1b6ab30a11
Niklas Lantau <niklaslantau@gmail.com>
2022-01-15 12:34:41 +0100

    update

---
+++ LeetCode/tech_interview_handbook/Extra_hand_of_straights.py
@@ -0,0 +1,67 @@
+# nlantau, 2022-01-15
+
+from typing import *
+from collections import Counter
+import heapq
+
+
+class Solution:
+    def isNStraightHand2(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0: return False
+
+        hs = {}
+        for d in hand:
+            hs[d] = 1 + hs.get(d, 0)
+
+        min_heap = list(hs.keys())
+        heapq.heapify(min_heap)
+
+        while min_heap:
+            min_val = min_heap[0]
+
+            for i in range(min_val, min_val + groupSize):
+                if i not in hs:
+                    return False
+                hs[i] -= 1
+                if hs[i] == 0:
+                    if i != min_heap[0]:
+                        return False
+                    heapq.heappop(min_heap)
+        return True
+
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0: return False
+
+        hs = Counter(hand)
+        min_heap = list(hs.keys())
+        heapq.heapify(min_heap)
+
+        while min_heap:
+            min_val = min_heap[0]
+
+            for i in range(min_val, min_val + groupSize):
+                if i not in hs: return False
+                hs[i] -= 1
+                if hs[i] == 0:
+                    if i != min_heap[0]: return False
+                    heapq.heappop(min_heap)
+        return True
+
+"""
+Notes:
+208 ms, 62.89% faster, 15.9 MB, 83.66% better
+
+"""
+
+if __name__ == "__main__":
+    r = [1,2,3,6,2,3,4,7,8]
+    b = 3
+    print(Solution().isNStraightHand(r, b)) # True
+
+    r = [1,2,3,6,2,3,4,7,3]
+    b = 3
+    print(Solution().isNStraightHand(r, b)) # False
+
+    r = [1,2,3,4,5]
+    b = 4
+    print(Solution().isNStraightHand(r, b)) # False
