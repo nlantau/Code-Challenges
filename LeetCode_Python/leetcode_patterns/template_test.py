3684893b7f39cd5e5c684e03ce0524ef4110242a
Niklas Lantau <niklaslantau@gmail.com>
2022-01-29 09:47:55 +0100

    update

---
+++ LeetCode_Python/leetcode_patterns/template_test.py
@@ -0,0 +1,18 @@
+
+from typing import *
+
+class Solution:
+    pass
+
+class TestClass:
+    def test_example_1(self):
+        pass
+
+    def test_example_2(self):
+        pass
+
+    def test_example_3(self):
+        pass
+
+if __name__ == "__main__":
+    pass
